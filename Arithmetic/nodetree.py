bf1b2812b5fc64f0ae7cad2125165fed35e19bbe
DC-Joney <1050617313@qq.com>
2017-09-01 17:21:32 +0800

    09-01

--- Arithmetic/nodetree.py
+++ Arithmetic/nodetree.py
@@ -68,6 +68,53 @@ class Tree(object):
                 myQueue.append(node.lchild)
             if node.rchild != None:
                 myQueue.append(node.rchild)
+    ###堆栈先序遍历
+
+    def front_stack(self,root):
+        if root == None:
+            return
+        myStack = []
+        node = root
+        while node or myStack:
+            while node:
+                print(node.elem)
+                myStack.append(node)
+                node = node.lchild
+            node = myStack.pop()
+            node =  node.rchild
+
+        ###堆栈中序遍历
+        def middle_stack(self, root):
+            if root == None:
+                return
+            myStack = []
+            node = root
+            while node or myStack:
+                while node:   #从根节点开始，一直找它的左子树
+                    print(node.elem)
+                    myStack.append(node)
+                    node = node.lchild
+                node = myStack.pop()  #while结束表示当前节点node为空，即前一个节点没有左子树了
+                node = node.rchild   #开始查看它的右子树
+
+        ##堆栈后序遍历
+        def later_stack(self,root):
+            if root == None:
+                return
+            myStack1 = []
+            myStack2 = []
+            node = root
+            myStack1.append(node)
+            while myStack1:  #这个while循环的功能是找出后序遍历的逆序，存在myStack2里面
+                node = myStack1.pop()
+                if node.lchild:
+                    myStack1.append(node.lchild)
+                if node.rchild:
+                    myStack1.append(node.rchild)
+                myStack2.append(node)
+            while myStack2:    #将myStack2中的元素出栈，即为后序遍历次序
+                print(myStack2.pop().elem)
+
 if __name__ == '__main__':
     """主函数"""
     elems = range(10)           #生成十个数据作为树节点
